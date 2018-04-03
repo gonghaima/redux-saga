ecba58ae75d0142af57cc69d48807ecf60a9d42f
steven <gonghaima@hotmail.com>
2018-04-03 12:17:25 +1000

    add saga

--- src/getStore.js
+++ src/getStore.js
@@ -10,7 +10,9 @@ import thunk from 'redux-thunk'
 
 import { getQuery } from './utility'
 import { reducer } from './combineReducers';
-import { defaultState } from './defaultState'
+import { defaultState } from './defaultState';
+import createSagaMiddleware from 'redux-saga';
+import { initSagas } from './initSagas';
 
 const stateTransformer = (state) => {
     if (Iterable.isIterable(state)) return state.toJS();
@@ -22,7 +24,9 @@ const logger = createLogger({
 });
 
 export const getStore = ()=>{
-    const middleWares = [thunk];
+    const sagaMiddleware = createSagaMiddleware();
+    if (getQuery()['logger']) { middleware, thunk};
+    const middleWares = [sagaMiddleware, thunk];
     if (getQuery()['logger']) { middleWares.push(logger)}
     const composables = [applyMiddleware(...middleWares)]
     const enhancer = compose(
@@ -33,5 +37,12 @@ export const getStore = ()=>{
         defaultState,
         enhancer
     );
+
+    console.log('====================================');
+    console.log("Saga middleware implemented.");
+    console.log('====================================');
+    
+    initSagas(sagaMiddleware);
+    
     return store;
 };
