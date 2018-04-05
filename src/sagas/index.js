c9c04297a44879655221752bd062dd7c14af060a
steven <gonghaima@hotmail.com>
2018-04-06 08:19:25 +1000

    Channels : Tax Rate Saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -5,3 +5,4 @@ export {itemQuantitySaga} from './itemQuantitySaga';
 export {itemPriceSaga} from './itemPriceSaga';
 export {customerServiceAvailabilitySaga} from './customerServiceAvailabilitySaga';
 export {shippingSaga} from './shippingSaga';
+export {taxRateSaga} from './taxRateSaga';
